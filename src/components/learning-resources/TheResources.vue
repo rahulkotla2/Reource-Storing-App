08de135082e09238e94890f51c4cb9412b6e9d8f
rahulkotla2 <rahulkotla2@gmail.com>
2023-01-30 15:14:43 +0530

    Added new components TheResources,AddResource and Adjusted some components initialisation placements

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,60 @@
+<template>
+    <base-card>
+        <base-button 
+        @click="setSelectedTab('stored-resources')" 
+        :mode="storedResButtonMode">
+        Stored Resources</base-button>
+        <base-button 
+        @click="setSelectedTab('add-resource')" 
+        :mode="addResourceButtonMode">
+        Add Resource</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+
+</template>
+
+<script>
+import StoredResources from "./StoredResources.vue"
+import AddResource from "./AddResource.vue"
+export default {
+    components: {
+        StoredResources,
+        AddResource
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources',
+            storedResources: [{
+                id: 'official-guide',
+                title: 'Official Guide',
+                description: 'The Official Vue.js documentation',
+                link: 'http://vuejs.org'
+            },
+            {
+                id: 'google',
+                title: 'Google',
+                description: 'Learn to google...',
+                link: 'http://google.org'
+            }],
+        }
+    },
+    provide() {
+        return {
+            resources: this.storedResources,
+        }
+    },
+    computed: {
+        storedResButtonMode() {
+            return this.selectedTab === 'stored-resources' ? null : 'flat'
+        },
+        addResourceButtonMode() {
+            return this.selectedTab === 'add-resource' ? null : 'flat'
+        },
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
